4961578fa3e3e67d24554fe2eff2f0d1519a2fac
Esteban Las <lasesteban@gmail.com>
2020-04-01 21:54:37 +0200

    get ready boy

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,21 +1,17 @@
-import React from 'react';
-import IconButton from '@material-ui/core/IconButton';
-import DeleteIcon from '@material-ui/icons/Delete';
-import Joke from './Joke'
+import React from "react";
+import Joke from "./Joke";
+import "./App.css";
 
-import './App.css';
-
-const Calendar = ({ days, cookies }) =>{
-
+const Calendar = ({ days }) => {
   return (
     <div>
       <div className="calendar">
         <div className="header">Days in Quarantine</div>
-        <span>{ days }</span>
+        <span>{days}</span>
       </div>
-      <Joke cookies={cookies}/>
+      <Joke />
     </div>
   );
-}
+};
 
 export default Calendar;
